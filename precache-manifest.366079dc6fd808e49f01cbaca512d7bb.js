46f0f30ae799e4739a13aca72f6d7e1020874fed
Raphael Ferreira de Moraes <raphaelfm.2012@gmail.com>
2022-12-15 12:55:27 -0300

    First commit

---
+++ precache-manifest.366079dc6fd808e49f01cbaca512d7bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3603dda89f1803a52582",
+    "url": "/static/css/main.25c94096.chunk.css"
+  },
+  {
+    "revision": "3603dda89f1803a52582",
+    "url": "/static/js/main.3603dda8.chunk.js"
+  },
+  {
+    "revision": "be45a15415ff8c878ea3",
+    "url": "/static/js/1.be45a154.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0d8389c6b21aa2941f1890e7e381b4b1",
+    "url": "/index.html"
+  }
+];
